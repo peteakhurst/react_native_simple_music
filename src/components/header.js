aa3ae1217259525a7dbda12231f781893c17f693
Pete Akhurst <peteakhurst@gmail.com>
2017-12-03 13:05:25 +1100

    first commit

---
+++ src/components/header.js
@@ -0,0 +1,38 @@
+// Import libraries to making a component
+  import React from 'react';
+  import { Text, View } from 'react-native';
+
+
+// Make the component
+
+const Header = (props) => {
+  const { textStyle, viewStyle } = styles;
+
+  return (
+    <View style={viewStyle}>
+      <Text style={textStyle}>{props.headerText}</Text>
+    </View>
+  );
+};
+
+const styles = {
+  viewStyle: {
+    backgroundColor: '#f8f8f8',
+    justifyContent: 'center',
+    alignItems: 'center',
+    height: 60,
+    paddingTop: 15,
+    shadowColor: '#000',
+    shadowOffset: { width: 0, height: 1 },
+    shadowOpacity: 0.2,
+    elevation: 2,
+    position: 'relative',
+  },
+  textStyle: {
+    fontSize: 20,
+    color: '#555'
+  },
+};
+
+// Make the component available to other parts of the app
+export default Header;
